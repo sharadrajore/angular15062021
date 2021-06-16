745f4d9601da11315a63c6ec682712d5df0ca615
sharad <sharad.rajore@gmail.com>
2021-06-16 22:06:54 +0530

    Angular application

---
+++ typescript/demo.js
@@ -0,0 +1,7 @@
+function print() {
+    for (var i = 1; i <= 5; i++) {
+        console.log(i);
+    }
+    console.log(i);
+}
+print();
---
+++ typescript/demo.ts
@@ -0,0 +1,13 @@
+
+
+function print(){
+
+    for(let i=1;i<=5;i++){
+        console.log(i);
+    }
+
+     console.log(i);
+}
+
+
+print();
